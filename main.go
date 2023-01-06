98fcbad331a814462708b7297b1f2600ca445781
Rashid Mamarimov <sonochiwaxd@gmail.com>
2023-01-07 01:33:24 +0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go-crud/controllers"
+	"go-crud/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/posts", controllers.PostsCreate)
+	r.PUT("/posts/:id", controllers.PostsUpdate)
+	r.GET("/posts", controllers.PostsIndex)
+	r.GET("/posts/:id", controllers.PostsShow)
+	r.DELETE("/posts/:id", controllers.PostsDelete)
+
+	r.Run() // localhost:3000
+}
